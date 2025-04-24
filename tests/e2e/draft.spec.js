28dc4110dee70535831db090de2d107d551182d3
Andrey Pozdnyakov <pozdnyakov.andrey.s@gmail.com>
2025-04-24 15:43:53 +0300

    Refactor E2E tests

--- tests/e2e/draft.spec.js
+++ tests/e2e/draft.spec.js
@@ -1,6 +1,5 @@
-// @ts-check
 import { expect, test } from "@playwright/test";
-import { createDraft, startDraft } from "./utilities";
+import { createDraft, fillGame, startDraft } from "./utilities";
 
 test("can start draft", async ({ page, context }) => {
 	const [blueSideUrl, redSideUrl, _spectatorUrl] = await createDraft(page);
@@ -8,106 +7,66 @@ test("can start draft", async ({ page, context }) => {
 	const blueSidePage = page;
 	await blueSidePage.goto(blueSideUrl);
 
-	expect(await confirmButtonState(blueSidePage)).toBe(
-		"awaits_ready_confirmation",
-	);
-
 	const redSidePage = await context.newPage();
 	await redSidePage.goto(redSideUrl);
 
-	expect(await confirmButtonState(redSidePage)).toBe(
-		"awaits_ready_confirmation",
-	);
+	for (const page of [blueSidePage, redSidePage]) {
+		await expect(page.getByRole("button", { name: "Ready" })).toBeVisible();
+	}
 
-	const blueSideConfirmButtonLocator =
-		await getConfirmButtonLocator(blueSidePage);
-	await blueSideConfirmButtonLocator.click();
-	expect(await confirmButtonState(blueSidePage)).toBe(
-		"awaits_red_confirmation",
-	);
+	await blueSidePage.getByRole("button", { name: "Ready" }).click();
+	await expect(
+		blueSidePage.getByRole("button", { name: "Waiting for Red..." }),
+	).toBeVisible();
 
-	const redSideConfirmButtonLocator =
-		await getConfirmButtonLocator(redSidePage);
-	await redSideConfirmButtonLocator.click();
-
+	await redSidePage.getByRole("button", { name: "Ready" }).click();
 	// TODO: Wait for visual confirmation instead
 	await redSidePage.waitForTimeout(100);
 
-	expect(await confirmButtonState(redSidePage)).toBe("awaits_champion_lock");
-	await expect(redSideConfirmButtonLocator).toBeDisabled();
-
-	expect(await confirmButtonState(blueSidePage)).toBe("awaits_champion_lock");
-	await expect(blueSideConfirmButtonLocator).toBeDisabled();
-
-	await expect(page.locator("#timer")).toBeVisible();
+	for (const page of [blueSidePage, redSidePage]) {
+		await expect(page.getByRole("button", { name: "Lock In" })).toBeVisible();
+		await expect(page.getByRole("button", { name: "Lock In" })).toBeDisabled();
+		await expect(page.locator("#timer")).toBeVisible();
+	}
 });
 
 test("hover state is broadcasted to all participants", async ({
 	page,
 	context,
 }) => {
-	const [blueSidePage, redSidePage, spectatorPage] = await startDraft(
-		page,
-		context,
-	);
+	const pages = await startDraft(page, context);
+	const [blueSidePage, _redSidePage, _spectatorPage] = pages;
 
-	expect(await blueSidePage.getByAltText("BB1").getAttribute("src")).toContain(
-		"placeholder.png",
-	);
-	expect(await redSidePage.getByAltText("BB1").getAttribute("src")).toContain(
-		"placeholder.png",
-	);
-	expect(await spectatorPage.getByAltText("BB1").getAttribute("src")).toContain(
-		"placeholder.png",
-	);
+	for (const page of pages) {
+		const iconSrc = await page.getByAltText("BB1").getAttribute("src");
+		expect(iconSrc).toContain("placeholder.png");
+	}
 
 	await blueSidePage.locator("#champion-grid").getByAltText("Annie").click();
 
-	expect(await blueSidePage.getByAltText("BB1").getAttribute("src")).toContain(
-		"1.png",
-	);
-	expect(await redSidePage.getByAltText("BB1").getAttribute("src")).toContain(
-		"1.png",
-	);
-
-	expect(await spectatorPage.getByAltText("BB1").getAttribute("src")).toContain(
-		"1.png",
-	);
+	for (const page of pages) {
+		const iconSrc = await page.getByAltText("BB1").getAttribute("src");
+		expect(iconSrc).toContain("1.png");
+	}
 });
 
 test("can switch sides", async ({ page, context }) => {
 	const pages = await startDraft(page, context);
 
-	const [blueSidePage, redSidePage, spectatorPage] = pages;
+	const [blueSidePage, redSidePage, _spectatorPage] = pages;
 
 	for (const page of pages) {
 		await expect(page.locator("#blue-team-name")).toHaveText("Team 1");
 		await expect(page.locator("#red-team-name")).toHaveText("Team 2");
 	}
 
-	await lockChampion(blueSidePage, "Annie");
-	await lockChampion(redSidePage, "Olaf");
-	await lockChampion(blueSidePage, "Galio");
-	await lockChampion(redSidePage, "Twisted Fate");
-	await lockChampion(blueSidePage, "Xin Zhao");
-	await lockChampion(redSidePage, "Urgot");
-
-	await lockChampion(blueSidePage, "Vladimir");
-	await lockChampion(redSidePage, "Fiddlesticks");
-	await lockChampion(redSidePage, "Kayle");
-	await lockChampion(blueSidePage, "Master Yi");
-	await lockChampion(blueSidePage, "Alistar");
-	await lockChampion(redSidePage, "Sivir");
-
-	await lockChampion(redSidePage, "Soraka");
-	await lockChampion(blueSidePage, "Teemo");
-	await lockChampion(redSidePage, "Tristana");
-	await lockChampion(blueSidePage, "Warwick");
-
-	await lockChampion(redSidePage, "Nunu & Willump");
-	await lockChampion(blueSidePage, "Miss Fortune");
-	await lockChampion(blueSidePage, "Ashe");
-	await lockChampion(redSidePage, "Tryndamere");
+	// biome-ignore format: lines reflect various pick/ban phases
+	await fillGame(blueSidePage, redSidePage, [
+		"Annie", "Olaf", "Galio", "Twisted Fate", "Xin Zhao", "Urgot",
+		"Vladimir", "Fiddlesticks", "Kayle", "Master Yi", "Alistar", "Sivir",
+		"Soraka", "Teemo", "Tristana", "Warwick",
+		"Nunu & Willump", "Miss Fortune", "Ashe", "Tryndamere"
+	]);
 
 	for (const page of [blueSidePage, redSidePage]) {
 		await expect(
@@ -140,37 +99,3 @@ test("can switch sides", async ({ page, context }) => {
 		await expect(page.locator("#red-team-name")).toHaveText("Team 1");
 	}
 });
-
-async function getConfirmButtonLocator(page) {
-	return await page.locator("#confirmButton");
-}
-
-async function lockChampion(page, championName) {
-	await page.locator("#champion-grid").getByAltText(championName).click();
-
-	// We expect button to already be visible. No need to wait for default timeout
-	await page.locator("#confirmButton").click({ timeout: 1000 });
-
-	// Server needs some time to process pick/ban
-	// TODO: Wait for visual confirmation instead
-	await page.waitForTimeout(50);
-}
-
-async function confirmButtonState(page) {
-	const confirmButtonLocator = await getConfirmButtonLocator(page);
-
-	const buttonLabel = await confirmButtonLocator.textContent();
-
-	switch (buttonLabel.toLowerCase()) {
-		case "ready":
-			return "awaits_ready_confirmation";
-		case "waiting for red...":
-			return "awaits_red_confirmation";
-		case "waiting for blue...":
-			return "awaits_blue_confirmation";
-		case "lock in":
-			return "awaits_champion_lock";
-		default:
-			return "unknown";
-	}
-}
--- tests/e2e/utilities.js
+++ tests/e2e/utilities.js
@@ -26,6 +26,36 @@ export async function startDraft(page, context) {
 	return [blueSidePage, redSidePage, spectatorPage];
 }
 
+// biome-ignore format: lines reflect various pick/ban phases
+const pickOrder = [
+	"BB1", "RB1", "BB2", "RB2", "BB3", "RB3",
+	"BP1", "RP1", "RP2", "BP2", "BP3", "RP3",
+	"RB4", "BB4", "RB5", "BB5",
+	"RP4", "BP4", "BP5", "RP5"
+]
+
+export async function fillGame(blueSidePage, redSidePage, picksBans) {
+	for (let i = 0; i < picksBans.length; i++) {
+		let page = blueSidePage;
+		if (pickOrder[i][0] === "R") {
+			page = redSidePage;
+		}
+
+		await lockChampion(page, picksBans[i]);
+	}
+}
+
+export async function lockChampion(page, championName) {
+	await page.locator("#champion-grid").getByAltText(championName).click();
+
+	// We expect button to already be visible. No need to wait for default timeout
+	await page.locator("#confirmButton").click({ timeout: 1000 });
+
+	// Server needs some time to process pick/ban
+	// TODO: Wait for visual confirmation instead
+	await page.waitForTimeout(50);
+}
+
 async function getDraftUrl(linkContainerLocator) {
 	return await linkContainerLocator.getByRole("link").getAttribute("href");
 }
